a1fe36ee510b6601cf4ad2e0b50cb334ec3a1384
Lara Poveda <lara.poveda@the-cocktail.com>
2018-09-20 15:45:30 +0200

    Create º

---
+++ KataLogInLogOutSwift/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  KataLogInLogOutSwift
+//
+//  Created by Lara Poveda on 20/9/18.
+//  Copyright © 2018 prueba. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
